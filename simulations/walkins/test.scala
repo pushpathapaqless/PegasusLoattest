02c8db7acfa15d89b4aace4b1f6c4d2039de7b27
pushpathapaqless <pushpa.thapa@qless.com>
2023-04-27 10:45:28 -0400

    added test file

---
+++ simulations/walkins/test.scala
@@ -0,0 +1,60 @@
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class AddWalkInsForToday extends Simulation{
+
+	val httpProtocol = http
+	    .baseUrl("https://apps-api.staging.peg.qless.com/api/v1")
+	    .acceptHeader("*/*")
+	    .contentTypeHeader("application/json")
+
+	val organizationId = "ORGBC52D7E91F3E4051973CD9A6CC74D4E1USEAST1"
+	val locationId = "LOC3477C5AAF32E45D59D812F6E7A405DA2USEAST1"
+	val serviceId = "SVCDDA21A92812D465D9F11FA2B9218BE03USEAST1"
+
+	val createWalkIn = http("reserveTimeSlot")
+				.post(s"/organizations/$organizationId/locations/$locationId/services/$serviceId/resources/walkins")
+				.body(StringBody("""{
+						"source" : "WEB_KIOSK"
+				}""")).asJson
+				.check(status.is(201))
+				.check(jsonPath("$.id").saveAs("walkInId"))
+
+
+	val updateWalkInFields = http("updateWalkInFields")
+				.put(s"/organizations/$organizationId/locations/$locationId/walkins/${"$walkInId"}/fields")
+				.body(StringBody("""{
+					"internalName" : "First Name", "values" : ["Steve"],
+					"internalName" : "Phone Number", "values" : ["+19196329575"],
+					"internalName" : "Email", "values" : ["pushpa.thapa@qless.com"]
+				}""")).asJson
+				.check(status.is(200))
+
+	val confirmWalkIn = http("confirmWalkIn")
+				.put(s"/organizations/$organizationId/locations/$locationId/resources/walkins/${"$walkInId"}")
+				.body(StringBody("""{
+					"action" : "SCHEDULE",
+					"source" : "WEB_KIOSK"
+				}""")).asJson
+				.check(status.is(200))
+
+
+	val scn = scenario("Create WalkIn")
+		.exec(createWalkIn)
+		.exec(session => {println(s"Created walkIn with id: ${session("walkInId").as[String]}")
+		session
+		})
+		.pause(1 seconds)
+		.exec(updateWalkInFields)
+		.exec(confirmWalkIn)
+
+	setUp(
+		scn.inject(
+			atOnceUsers(1)
+		)
+	).protocols(httpProtocol)
+}
